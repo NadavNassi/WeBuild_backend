f3ff978431a9ffe1364e4ffc50980491d6fa80d5
Nadav Nassi <nadav.nassi@gmail.com>
2021-06-01 22:13:54 +0300

    atlas

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'dbURL': 'mongodb://localhost:27017',
-    // 'dbURL': 'mongodb+srv://nadav:nadav@cluster0.xt36l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    // 'dbURL': 'mongodb://localhost:27017',
+    'dbURL': 'mongodb+srv://nadav:nadav@webuild.x31ee.mongodb.net/WeBuild?retryWrites=true&w=majority',
 }
